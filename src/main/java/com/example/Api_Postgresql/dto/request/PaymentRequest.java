ce464dd38b74aca46f670ca7b39f0f053876bcf6
Alisson1471 <alissonalvesdossantosoliveira@gmail.com>
2025-09-30 12:38:08 -0300

    feat: add create payment endpoint

---
+++ src/main/java/com/example/Api_Postgresql/dto/request/PaymentRequest.java
@@ -0,0 +1,34 @@
+package com.example.Api_Postgresql.dto.request;
+
+import com.example.Api_Postgresql.validation.OnCreate;
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotNull;
+import lombok.*;
+
+import java.time.LocalDate;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@ToString
+public class PaymentRequest {
+
+    @NotNull(message = "field 'userType' is null", groups = OnCreate.class)
+    private String userType;
+
+    @NotNull(message = "field 'userId' is null", groups = OnCreate.class)
+    @Min(value = 0, message = "'UserId' can't be less than 1")
+    private Integer userId;
+
+    @NotNull(message = "field 'planoId' is null", groups = OnCreate.class)
+    @Min(value = 0, message = "'PlanId' can't be less than 1")
+    private Integer planId;
+
+    @NotNull(message = "field 'paidDate' is null", groups = OnCreate.class)
+    private LocalDate paidDate;
+
+    @NotNull(message = "field 'frequency' is null", groups = OnCreate.class)
+    private String frequency;
+
+}
---
+++ src/main/java/com/example/Api_Postgresql/mapper/PaymentMapper.java
@@ -0,0 +1,86 @@
+package com.example.Api_Postgresql.mapper;
+
+import com.example.Api_Postgresql.dto.request.PaymentRequest;
+import com.example.Api_Postgresql.dto.response.PaymentResponse;
+import com.example.Api_Postgresql.dto.response.PaymentResponse.UserInfo;
+import com.example.Api_Postgresql.dto.response.PaymentResponse.PlanInfo;
+import com.example.Api_Postgresql.model.Company;
+import com.example.Api_Postgresql.model.Payment;
+import com.example.Api_Postgresql.model.Plan;
+import com.example.Api_Postgresql.model.Worker;
+import com.example.Api_Postgresql.repository.CompanyRepository;
+import com.example.Api_Postgresql.repository.PlanRepository;
+import com.example.Api_Postgresql.repository.WorkerRepository;
+import jakarta.persistence.EntityNotFoundException;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Component;
+
+@RequiredArgsConstructor
+@Component
+public class PaymentMapper {
+
+    private final PlanRepository planRepository;
+
+    private final CompanyRepository companyRepository;
+
+    private final WorkerRepository workerRepository;
+
+    public Payment toPayment(PaymentRequest request) {
+
+        Plan plan = planRepository.findById(request.getPlanId())
+                .orElseThrow(() -> new EntityNotFoundException("Plan with id "+ request.getPlanId() +" not found"));
+
+
+        if (request.getUserType().equals("worker")) {
+            Worker worker = workerRepository.findById(request.getUserId())
+                    .orElseThrow(() ->  new EntityNotFoundException("Worker with id "+ request.getUserId() +" not found"));
+
+            return new Payment(
+                    worker,
+                    plan,
+                    plan.getValue(),
+                    request.getPaidDate(),
+                    request.getFrequency()
+            );
+        } else {
+            Company company = companyRepository.findById(request.getUserId())
+                    .orElseThrow(() ->  new EntityNotFoundException("Company with id "+ request.getUserId() +" not found"));
+
+            return new Payment(
+                    company,
+                    plan,
+                    plan.getValue(),
+                    request.getPaidDate(),
+                    request.getFrequency()
+            );
+        }
+    }
+
+    public PaymentResponse toPaymentResponse(Payment payment) {
+        UserInfo userInfo = new UserInfo();
+        if (payment.getCompany() != null) {
+            userInfo.setId(payment.getCompany().getId());
+            userInfo.setType("company");
+            userInfo.setName(payment.getCompany().getName());
+        } else {
+            userInfo.setId(payment.getWorker().getId());
+            userInfo.setType("worker");
+            userInfo.setName(payment.getWorker().getName());
+        }
+
+        PlanInfo planInfo = new PlanInfo(
+                payment.getPlan().getId(),
+                payment.getPlan().getName(),
+                payment.getAmount()
+        );
+
+         return new PaymentResponse(
+                payment.getId(),
+                userInfo,
+                planInfo,
+                payment.getPaidDate(),
+                payment.getFrequency()
+        );
+    }
+
+}
