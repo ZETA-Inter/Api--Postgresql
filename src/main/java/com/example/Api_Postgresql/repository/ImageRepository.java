2998bbd735fe8fc4b613f09dac270f1a2fc9e320
Alisson1471 <alissonalvesdossantosoliveira@gmail.com>
2025-10-06 10:32:53 -0300

    feat: add field imagemUrl in the program response

--- src/main/java/com/example/Api_Postgresql/repository/ImageRepository.java
+++ src/main/java/com/example/Api_Postgresql/repository/ImageRepository.java
@@ -3,5 +3,10 @@ package com.example.Api_Postgresql.repository;
 import com.example.Api_Postgresql.model.Image;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface ImageRepository extends JpaRepository<Image, Integer> {
+
+    Image findImageBySourceIdAndOriginTable(Integer sourceId, String originTable);
+
 }
