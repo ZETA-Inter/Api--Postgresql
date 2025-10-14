fb672c09a9d0e65c68cac601fe2dc5802695f6a0
Alisson1471 <alissonalvesdossantosoliveira@gmail.com>
2025-10-14 08:35:04 -0300

    feat: add goals progress endpoint

--- src/main/java/com/example/Api_Postgresql/mapper/GoalMapper.java
+++ src/main/java/com/example/Api_Postgresql/mapper/GoalMapper.java
@@ -1,6 +1,7 @@
 package com.example.Api_Postgresql.mapper;
 
 import com.example.Api_Postgresql.dto.request.GoalRequestDTO;
+import com.example.Api_Postgresql.dto.response.GoalProgressResponseDTO;
 import com.example.Api_Postgresql.dto.response.GoalResponseDTO;
 import com.example.Api_Postgresql.model.Goal;
 import org.springframework.stereotype.Component;
@@ -17,6 +18,13 @@ public class GoalMapper {
                 goal.getId(),
                 goal.getDescription()
         );
+    }
+
+    public GoalProgressResponseDTO convertToGoalProgressResponseDTO(int count, double progressPercentage) {
+        return GoalProgressResponseDTO.builder()
+                .count(count)
+                .progressPercentage(progressPercentage)
+                .build();
     }
 
 }
