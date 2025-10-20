86135648732b7fef0f7348c137e9f108c9c15bb2
Alisson1471 <alissonalvesdossantosoliveira@gmail.com>
2025-10-20 08:21:28 -0300

    fix: adjust the CORS to accept anyone request origin

--- src/main/java/com/example/Api_Postgresql/config/CorsConfig.java
+++ src/main/java/com/example/Api_Postgresql/config/CorsConfig.java
@@ -14,9 +14,7 @@ public class CorsConfig {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
                 registry.addMapping("/**")
-                        .allowedOrigins(
-                                "http://localhost:5173"
-                        )
+                        .allowedOriginPatterns("*")
                         .allowedMethods("GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS")
                         .allowedHeaders("*")
                         .allowCredentials(true);
