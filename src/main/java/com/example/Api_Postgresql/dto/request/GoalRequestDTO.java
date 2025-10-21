9ae3d67a72df02912f835fd72bcdd429dcc40558
Alisson1471 <alissonalvesdossantosoliveira@gmail.com>
2025-10-21 07:58:58 -0300

    fix: adjust field 'programId' to request can be null

--- src/main/java/com/example/Api_Postgresql/dto/request/GoalRequestDTO.java
+++ src/main/java/com/example/Api_Postgresql/dto/request/GoalRequestDTO.java
@@ -4,6 +4,7 @@ import com.example.Api_Postgresql.validation.OnCreate;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import jakarta.validation.constraints.Min;
 import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Null;
 import jakarta.validation.constraints.Size;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
@@ -27,7 +28,6 @@ public class GoalRequestDTO {
     private Integer companyId;
 
     @JsonProperty("program_id")
-    @NotNull(message = "field 'programId' is null", groups = OnCreate.class)
     @Min(value = 1, message = "'programId' can't be less than 1", groups = OnCreate.class)
     private Integer programId;
 
